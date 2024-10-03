dca22da8eb6a11dd20a71debd840c14416bd3c72
Rafsan The General <rafsanthegeneral@gmail.com>
2024-10-04 05:55:02 +0600

    Folder Modification

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-# usr/bin/python
-import httpx
-import os
 
-while 1:
-    inp = input("[+]Input your Command :")
-    #print (httpx.get("https://57f1-103-161-69-252.ngrok-free.app"))
-    print (os.system(inp))
+print ("Hello wordl")
